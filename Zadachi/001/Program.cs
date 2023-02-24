979e8d8c10af51597b5019ad9515b79984307db5
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-24 19:11:54 +0200

    альтернативное решение++

--- Zadachi/001/Program.cs
+++ Zadachi/001/Program.cs
@@ -6,6 +6,8 @@ int userNumber = int.Parse(userInputStr);
 
 //int userNumber = int.Parse(Console.ReadLine() ?? "");
 
+//int result = (int)Math.Pow(userNumber, 2);
+
 int result = userNumber * userNumber;
 
 Console.WriteLine($"{userNumber} -> {result}");
