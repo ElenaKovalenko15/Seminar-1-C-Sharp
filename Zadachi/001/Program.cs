6050956ff0dbaf4574e604b735ef39da7a32d0bb
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-24 17:32:08 +0200

    Kvadrat-1

---
+++ Zadachi/001/Program.cs
@@ -0,0 +1,9 @@
+﻿//Квадрат числа возвращает
+Console.Write("Введите целое число: ");
+
+string userInputStr = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInputStr);
+
+int result = userNumber * userNumber;
+
+Console.WriteLine(result);
