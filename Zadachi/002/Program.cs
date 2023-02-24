5cfca2fdae18eb97b08d855464e5413870e22051
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-24 22:15:05 +0200

    Задача о проверке двух чисел

---
+++ Zadachi/002/Program.cs
@@ -0,0 +1,14 @@
+﻿// на вход принимает два числа и проверяет, является ли первое квадратом второго. а=25 в=5 -> yes; a=2,b=10 ->yes;a=9 b=-3 -> no; a=-3 b=9 -> no
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA==numB*numB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
