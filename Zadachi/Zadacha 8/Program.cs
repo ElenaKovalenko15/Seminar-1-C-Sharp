f9bd946ed0c712a33bc526b4fa61749271fdaa62
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-27 23:03:20 +0200

    Отбираем все четные до N

---
+++ Zadachi/Zadacha 8/Program.cs
@@ -0,0 +1,7 @@
+﻿// на вход принимает число N и выдает все четные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+    if (i % 2 == 0)
+    Console.WriteLine(i);
