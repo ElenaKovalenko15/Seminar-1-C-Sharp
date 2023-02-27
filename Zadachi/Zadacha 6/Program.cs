da2475ceba9275444bc2ec1220d2033b7d7eff33
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-27 22:17:47 +0200

    Оцениваем четное/нечетное

---
+++ Zadachi/Zadacha 6/Program.cs
@@ -0,0 +1,7 @@
+﻿// на вход принимает число и выдает, является ли число четным.
+Console.WriteLine("Введите число: ");
+int UserNum = Convert.ToInt32(Console.ReadLine());
+
+if (UserNum % 2 == 0)
+    Console.Write("-> четное");
+else Console.Write("-> нечетное");
