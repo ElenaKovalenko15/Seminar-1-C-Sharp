bc4851278c6ac73d1449fd00678beb79662acfb2
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-27 12:13:57 +0200

    Сравниваем 3 числа

---
+++ Zadachi/Zadacha 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимает три числа и выдвет максимальное их них
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (max < numB) max=numB;
+if (max < numC) max=numC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
