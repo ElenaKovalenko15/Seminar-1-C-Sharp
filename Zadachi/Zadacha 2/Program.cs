e675d47795694027dc8cf1feb8930830555f1e20
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-02-27 11:45:18 +0200

    Сравниваем 2 числа

---
+++ Zadachi/Zadacha 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.Write("Введите число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int numB = int.Parse(Console.ReadLine());
+
+int max = numA;
+
+if (max > numB);
+else
+{
+    max = numB;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
