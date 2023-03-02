30027832d2817338d080406b2329dc6ff5240bd4
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-02 20:58:59 +0200

    Задача 10

---
+++ Zadachi/Zadacha 10/Program.cs
@@ -0,0 +1,9 @@
+﻿// принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int Num = int.Parse(Console.ReadLine());
+
+int a2 = (Num / 10) % 10;
+
+Console.WriteLine($"Вторая цифра числа {Num} -> {a2}");
