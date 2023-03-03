ed8bb315bf9175af77f5bfb4905ae16e8c4c88db
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-03 17:26:03 +0200

    Задача 15

---
+++ Zadachi/Zadacha 15/Program.cs
@@ -0,0 +1,25 @@
+﻿// принимает на вход цифру, обозначающую день недели, и прооверяет, является ли этот деньвыходным.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int Den = int.Parse(Console.ReadLine());
+
+if ((Den > 7) || (Den < 1))
+{
+    Console.WriteLine("Неверное число");
+
+}
+else
+{
+    bool resualt = (Den == 6) || (Den == 7);
+
+    if (resualt)
+    {
+        Console.WriteLine($"День недели с порядковым номером {Den} -> выходной.");
+    }
+
+    else
+    {
+        Console.WriteLine($"День недели с порядковым номером {Den} -> будний.");
+    }
+}
