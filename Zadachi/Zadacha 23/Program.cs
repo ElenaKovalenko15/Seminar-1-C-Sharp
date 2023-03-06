3fef18901f94e734570120fab1a672275f16a07b
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-06 23:31:50 +0200

    Принимает на вход число N и выдает таблицу кубов чисел от 1 до N

--- Zadachi/Zadacha 23/Program.cs
+++ Zadachi/Zadacha 23/Program.cs
@@ -1,48 +1,46 @@
-﻿// Принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+﻿Console.Clear();
 
-Console.Clear();
+ int N = ReadNumber("Введите число: ");
+ int[] array = new int[N + 1];
 
-int N = ReadNumber("Введите число: ");
-int[] array = new int[N + 1];
+ CubeNum(array);
 
-CubeNum(array);
+ PrintArray(array);
 
-PrintArray(array);
+ static int ReadNumber(string Message)
+ {
+     int n = 0;
+     while (true)
+     {
+         try
+         {
+             Console.Write(Message);
+             n = int.Parse(Console.ReadLine() ?? "");
+             break;
+         }
+         catch (Exception exc)
+         { Console.WriteLine("Ошибка ввода"); }
+     }
+     return n;
+ }
 
-static int ReadNumber(string Message)
-{
-    int n = 0;
-    while (true)
-    {
-        try
-        {
-            Console.Write(Message);
-            n = int.Parse(Console.ReadLine() ?? "");
-            break;
-        }
-        catch (Exception exc)
-        { Console.WriteLine("Ошибка ввода"); }
-    }
-    return n;
-}
-
-void CubeNum(int[] CubeNum)
-{
-    int count = 0;
-    int length = CubeNum.Length;
-    while (count < length)
-    {
-        CubeNum[count] = Convert.ToInt32(Math.Pow(count, 3));
-        count++;
-    }
-}
+static void CubeNum(int[] CubeNum)
+ {
+     int count = 0;
+     int length = CubeNum.Length;
+     while (count < length)
+     {
+         CubeNum[count] = Convert.ToInt32(Math.Pow(count, 3));
+         count++;
+     }
+ }
 
 
-void PrintArray(int[] array)
-{
-    int spread = array.Length;
-    for (int i = 0; i < spread; i++)
-    {
-        Console.Write(array[i]+ " ");
-    }
-}
+ static void PrintArray(int[] array)
+ {
+     int spread = array.Length;
+     for (int i = 0; i < spread; i++)
+     {
+         Console.Write(array[i]+ " ");
+     }
+ }
