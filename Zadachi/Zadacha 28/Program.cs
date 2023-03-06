245777ffda77fd5f2261a7cda6b1c910d9e0f017
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-06 20:10:31 +0200

    Задача 28/число N и выдает произведение от 1 до N )

---
+++ Zadachi/Zadacha 28/Program.cs
@@ -0,0 +1,29 @@
+﻿// Принимает число N и выдает произведение от 1 до N
+
+int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+int powNumbers = GetPowNumbers(num);
+Console.WriteLine($"{num} -> {powNumbers}");
+
+static int GetPowNumbers(int number)
+{
+    int pow = 1;
+
+while(number > 0)
+{
+    pow *= number;
+    number--;
+}
+    return pow;
+}
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
