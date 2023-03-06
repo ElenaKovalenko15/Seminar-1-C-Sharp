e37c83ea469842c6028049759bebb44e42ee461d
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-06 20:14:05 +0200

    принимает на вход число и выдает количество цифр в числе

---
+++ Zadachi/Sem4_001/Program.cs
@@ -0,0 +1,38 @@
+﻿//сумма чисел от 1 до Н
+
+// Input
+int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+
+//Logic
+int sumNumbers = GetSumNumbers(num);
+
+
+//Output
+
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+
+////////////////////////////////////////////////////
+static int GetSumNumbers(int number)
+{
+    int sum = 0;
+
+while(number > 0)
+{
+    sum += number;
+    number--;
+}
+    return sum;
+}
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
