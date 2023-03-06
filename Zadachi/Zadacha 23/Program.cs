8e157211c6bcfae547129b4af0d80c4d39d4d934
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-06 23:07:39 +0200

    Принимает на вход число N и выдает таблицу кубов чисел от 1 до N

---
+++ Zadachi/Zadacha 23/Program.cs
@@ -0,0 +1,48 @@
+﻿// Принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int N = ReadNumber("Введите число: ");
+int[] array = new int[N + 1];
+
+CubeNum(array);
+
+PrintArray(array);
+
+static int ReadNumber(string Message)
+{
+    int n = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write(Message);
+            n = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (Exception exc)
+        { Console.WriteLine("Ошибка ввода"); }
+    }
+    return n;
+}
+
+void CubeNum(int[] CubeNum)
+{
+    int count = 0;
+    int length = CubeNum.Length;
+    while (count < length)
+    {
+        CubeNum[count] = Convert.ToInt32(Math.Pow(count, 3));
+        count++;
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    int spread = array.Length;
+    for (int i = 0; i < spread; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
