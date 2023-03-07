7eb6f7eff909cf4e7da5d47a5b248cc247edab30
Kovalenko Elena Admin <Kovalenko.elena.mikh@gmail.com>
2023-03-07 10:33:00 +0200

    Проверяет, является ли пятизначное число палиндромом

---
+++ Zadachi/Zadacha 19/Program.cs
@@ -0,0 +1,43 @@
+﻿//принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+
+int num = ReadNumber("Введите пятизначное число: ");
+//Palindrom(??; ??);
+
+static int ReadNumber(string Message)
+{
+    int n = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write(Message);
+            n = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (Exception exc)
+        { Console.WriteLine("Ошибка ввода"); }
+    }
+    return n;
+}
+
+//static void Palindrom(string[] args)
+{
+    int n = 0;
+    int sum = 0;
+    int rev = 0;
+    int temp = n;
+    while (n > 0)
+    {
+        rev = n % 10;
+        sum = (sum * 10) + rev;
+        n = n / 10;
+    }
+    {
+        if (temp == sum)
+            Console.WriteLine("Число - палиндром.");
+        else
+            Console.WriteLine("Число - не палиндром.");
+    }
+}
